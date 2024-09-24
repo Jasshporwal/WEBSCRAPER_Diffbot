eff46ae6b96ce2c1195c5a38576ba2a2cd212796
Jassh <jassh.porwal@contentstack.com>
2024-09-24 11:57:00 +0530

    images

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ async def extract_article(url: HttpUrl):
 async def extract_product(url: HttpUrl):
     return product.extract_product(url)
 
-@app.get("/images", response_model=list[ImageData])  # Change here
+@app.get("/images",response_model=ImageData)
 async def extract_images(url: HttpUrl):
     return image.extract_images(url)
 
